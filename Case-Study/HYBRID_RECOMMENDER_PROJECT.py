bcf1135e0e87286228bf595b6ab7bcb111be41c2
Melisa Çevik <celissamevik@gmail.com>
2024-02-20 16:18:45 +0300

    add casestudy2

--- Case-Study/HYBRID_RECOMMENDER_PROJECT.py
+++ Case-Study/HYBRID_RECOMMENDER_PROJECT.py
@@ -37,7 +37,7 @@ comment_counts = pd.DataFrame(df["title"].value_counts())
 # Toplam oy kullanılma sayısı 1000'in altında olan filmlerin isimlerini rare_movies de tutuyoruz.
 # Ve veri setinden çıkartıyoruz
 
-rare_movies = comment_counts[comment_counts["count"] <= 10000].index #1000'in altındaki izlenmeler
+rare_movies = comment_counts[comment_counts["count"] <= 10000].index #10000'in altındaki izlenmeler(yorumlar)
 
 common_movies = df[~df["title"].isin(rare_movies)] # rare movies i liste dışı bırak
 
@@ -61,7 +61,7 @@ def create_user_movie_df():
 
     return user_movie_df
 
-user_movie_df = create_user_movie_df() #filtrelenen pivot tablomuz
+user_movie_df = create_user_movie_df() # 10000'den yukarıda rate i olan filtrelenmiş pivot tablomuz
 
 #############################################
 # Görev 2: Öneri Yapılacak Kullanıcının İzlediği Filmlerin Belirlenmesi
@@ -73,14 +73,16 @@ random_user = int(pd.Series(user_movie_df.index).sample(1).values) # dataframe'i
 
 
 # Adım 2: Seçilen kullanıcıya ait gözlem birimlerinden oluşan random_user_df adında yeni bir dataframe oluşturunuz.
-# random_user = 125773
+# random_user = 109655 ilk kullanım
+# random_user = 46691
 #user_movie_df
 
 random_user_df = user_movie_df[user_movie_df.index == random_user] # random kullanıcımı seçtim
 
 # Adım 3: Seçilen kullanıcının oy kullandığı filmleri movies_watched adında bir listeye atayınız.
 
-movies_watched = random_user_df.columns[random_user_df.notna().any()].tolist() # sütunlarda herhangi NA olmayan değerleri aldık yani random kullanıcının İZLEDİĞİ filmlere git
+movies_watched = random_user_df.columns[random_user_df.notna().any()].tolist() # sütunlarda herhangi NA olmayan değerleri aldık,
+# random kullanıcının İZLEDİĞİ filmleri movies_watched'a atadık.
 
 #############################################
 # Görev 3: Aynı Filmleri İzleyen Diğer Kullanıcıların Verisine ve Id'lerine Erişmek
@@ -88,15 +90,15 @@ movies_watched = random_user_df.columns[random_user_df.notna().any()].tolist() #
 
 # Adım 1: Seçilen kullanıcının izlediği fimlere ait sutunları user_movie_df'ten seçiniz ve movies_watched_df adında yeni bir dataframe oluşturuyoruz.
 
-movies_watched_df = user_movie_df[movies_watched] #genel df içerisindeki random kullanıcını izlediği filmleri al
+movies_watched_df = user_movie_df[movies_watched] # user_movie_df'in sütunlarını filtreledik
 
 # Adım 2: Herbir kullancının seçili user'in izlediği filmlerin kaçını izlediği bilgisini taşıyan user_movie_count adında yeni bir dataframe oluşturunuz.
 # Ve yeni bir df oluşturuyoruz.
 
-# kullanıcıların random ile ortak izledikleri filmlerin toplamı
+# kullanıcıların random ile ortak izledikleri filmlerin toplamı, 1 ortak olursa almalı mıyız?
 user_movie_count = movies_watched_df.T.notnull().sum() # notnull() => true false döner => bu dönüşümün trueları sum ediliyor (1)
 
-# movies_watched_df.T => dataframein satırları(userId) sütuna geçecek
+# T => dataframein satırları(userId) sütuna geçecek
 
 
 # Adım 3: Seçilen kullanıcının oy verdiği filmlerin yüzde 60 ve üstünü izleyenleri benzer kullanıcılar olarak görüyoruz.
@@ -107,39 +109,52 @@ user_movie_count.columns = ["userId", "movie_count"]
 
 users_same_movies = user_movie_count[user_movie_count["movie_count"] > len(movies_watched) * 60 / 100]["userId"].tolist()
 
+# random ile benzer davranışları gösteren user'ları list halinde tuttuk
 
 #############################################
 # Görev 4: Öneri Yapılacak Kullanıcı ile En Benzer Kullanıcıların Belirlenmesi
 #############################################
 
 # Adım 1: user_same_movies listesi içerisindeki seçili user ile benzerlik gösteren kullanıcıların id’lerinin bulunacağı şekilde movies_watched_df dataframe’ini filtreleyiniz.
-
 final_df = pd.concat([movies_watched_df[movies_watched_df.index.isin(users_same_movies)],
                       random_user_df[movies_watched]])
 
+# concat => df birleştirmek için => korelasyonlarına bakmak için birleştirdik!
+# 1. izlenen filmlerin indexlerinde benzer kullanıcıları ara
+# 2. random user 'ı da satır olarak birleştir
+# özetle random kullanıcının pivot df i ile bütün kullanıcıların df 'ini birleştirdik
+
+
 # Adım 2: Kullanıcıların birbirleri ile olan korelasyonlarının bulunacağı yeni bir corr_df dataframe’i oluşturunuz.
+
+# hesaplama yapmak için user'ları sütuna alıcaz.
 
 final_df = final_df.drop_duplicates() # tekrar eden
 corr_df = final_df.T.corr().unstack().sort_values().drop_duplicates()
 
-corr_df = pd.DataFrame(corr_df, columns=["corr"])
-corr_df.index.names = ['user_id_1', 'user_id_2']
-corr_df = corr_df.reset_index()
+corr_df = pd.DataFrame(corr_df, columns=["corr"]) # okunmadığı için DF ' e çevirdik
+corr_df.index.names = ['user_id_1', 'user_id_2'] #bunlar index isimleri değişkene çevirmek için;
+corr_df = corr_df.reset_index()  # bunu yaptık
 
+# bunlar tüm kullanıcılar açısından korelasyonlar
 
 # Adım 3: Seçili kullanıcı ile yüksek korelasyona sahip (0.65’in üzerinde olan) kullanıcıları filtreleyerek top_users adında yeni bir dataframe oluşturunuz.
 
 top_users = corr_df[(corr_df["user_id_1"] == random_user) & (corr_df["corr"] >= 0.65)][
-    ["user_id_2", "corr"]].reset_index(drop=True)
+    ["user_id_2", "corr"]].reset_index(drop=True) # "user_id_2", "corr" 'u seçiyorum , user_id_1 'e ihtiyacım yok
 
-# Adım 4:  top_users dataframe’ine rating veri seti ile merge ediniz
+top_users = top_users.sort_values(by="corr", ascending = False)
 
 top_users.rename(columns={"user_id_2": "userId"}, inplace=True) #user_id_2'nin adını değiştid
 
+# burada - leri almamam gerekiyor çünkü + korelasyon => randomla aynı artma azalma davranışı gösteriyor olması demektir
+
+# Adım 4:  top_users dataframe’ine rating veri seti ile merge ediniz
 
 rating = pd.read_csv('datasets/movie_lens_dataset/rating.csv')
 top_users_ratings = top_users.merge(rating[["userId", "movieId", "rating"]], how='inner')
-top_users_ratings = top_users_ratings[top_users_ratings["userId"] != random_user] #randomu buradan cıkardık
+
+# top_users_ratings = top_users_ratings[top_users_ratings["userId"] != random_user] #randomu buradan cıkardık
 
 
 #############################################
@@ -149,11 +164,17 @@ top_users_ratings = top_users_ratings[top_users_ratings["userId"] != random_user
 # Adım 1: Her bir kullanıcının corr ve rating değerlerinin çarpımından oluşan weighted_rating adında yeni bir değişken oluşturunuz.
 top_users_ratings['weighted_rating'] = top_users_ratings['corr'] * top_users_ratings['rating']
 
+# ratinge göre sıralarsak random da beğenebilir ama sadece rating i alırsak korelasyonu gözden kaçırırız ( korelasyonu aynı olduğunu varsaymış oluruz )
+# sadece korelasyona göre de alamayız cünkü bazı ratingler düşük
+# corr ve ratingin etkisini aynı anda göz önünde bulundurmalıyız.
 
 # Adım 2: Film id’sive her bir filme ait tüm kullanıcıların weighted rating’lerinin ortalama değerini içeren recommendation_df adında yeni bir
 # dataframe oluşturunuz.
-recommendation_df = top_users_ratings.groupby('movieId').agg({"weighted_rating": "mean"}).reset_index()
 
+recommendation_df = top_users_ratings.groupby('movieId').agg({"weighted_rating": "mean"}).reset_index()
+# movieId'ye birçok kullanıcı puan vermiş olabilir
+
+#randomun filmlere vereceği değerler ( düşük olanlar da var )
 
 # Adım 3: Adım3: recommendation_df içerisinde weighted rating'i 3.5'ten büyük olan filmleri seçiniz ve weighted rating’e göre sıralayınız.
 # İlk 5 gözlemi movies_to_be_recommend olarak kaydediniz.
@@ -172,20 +193,31 @@ movies_to_be_recommend.merge(movie[["movieId", "title"]]).head()
 # Kullanıcının en son izlediği ve en yüksek puan verdiği filmin adına göre item-based öneri yapınız.
 user = 108170
 
+user_id_data_type = user_movie_df.index.dtype
+print("userId sütununun veri tipi:", user_id_data_type)
 
 # Adım 1: movie,rating veri setlerini okutunuz.
 movie = pd.read_csv("Case-Study/datasets/movie.csv")
 rating = pd.read_csv("Case-Study/datasets/rating.csv")
+
+df = rating.merge(movie, how="left", on="movieId")
+
 # Adım 2: Öneri yapılacak kullanıcının 5 puan verdiği filmlerden puanı en güncel olan filmin id'sinin alınız.
 
 user_info = df[df["userId"] == 108170]
-latest_watched = user_info.loc[user_info["timestamp"] == user_info["timestamp"].max()]
-latest_highest_rating = user_info.loc[user_info["rating"] == user_info["rating"].max()]
 
-latest_highest_rated_date = latest_highest_rating.loc[latest_highest_rating["timestamp"].idxmax()]
+latest_watched = df[(df['userId'] == 108170) & (df['rating'] == 5)].sort_values(by='timestamp', ascending=False).drop_duplicates(subset='movieId')
+#birden fazla puan verme iht. karşı drop_duplicates yapıyoruz.
+
+most_latest_watched_id = latest_watched["movieId"].values[0]
+
+most_latest_movie_df = latest_watched.head(1)
 
 # Adım 3 :User based recommendation bölümünde oluşturulan user_movie_df dataframe’ini seçilen film id’sine göre filtreleyiniz.
 # 7044
+
+user_movie_df_filtered = df[df['movieId'] == most_latest_watched_id]
+other_movies_df = df[df['movieId'] != most_latest_watched_id]
 
 # Adım 4: Filtrelenen dataframe’i kullanarak seçili filmle diğer filmlerin korelasyonunu bulunuz ve sıralayınız.
 
